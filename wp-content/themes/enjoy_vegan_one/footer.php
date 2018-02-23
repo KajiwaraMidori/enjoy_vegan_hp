609b32b7526e7ce08c710a002ea4464b67b1e7d3
TsubasaK111 <Tsubasa.K111@gmail.com>
2018-02-23 21:40:51 +0900

    feat: add 'enjoy vegan one' theme (a underscore based theme)

---
+++ wp-content/themes/enjoy_vegan_one/footer.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package enjoy_vegan_one
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'enjoy_vegan_one' ) ); ?>"><?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly powered by %s', 'enjoy_vegan_one' ), 'WordPress' );
+			?></a>
+			<span class="sep"> | </span>
+			<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'enjoy_vegan_one' ), 'enjoy_vegan_one', '<a href="http://underscores.me/">Underscores.me</a>' );
+			?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
