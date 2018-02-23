edd53707cef036dbe0a368836d8b82727f849d85
TsubasaK111 <Tsubasa.K111@gmail.com>
2018-02-24 00:46:32 +0900

    feat: display header background image

--- wp-content/themes/enjoy_vegan_one/inc/custom-header.php
+++ wp-content/themes/enjoy_vegan_one/inc/custom-header.php
@@ -19,62 +19,35 @@
 	 */
 	function enjoy_vegan_one_custom_header_setup() {
 		add_theme_support( 'custom-header', apply_filters( 'enjoy_vegan_one_custom_header_args', array(
-			'default-image'          => get_parent_theme_file_uri( '/images/1_header/japan_map.png' ),
+			'default-image'          => get_template_directory_uri() . '/images/1_header/japan_map.png',
 			'default-text-color'     => '000000',
 			'width'                  => 1000,
 			'height'                 => 250,
 			'flex-height'            => true,
 			'wp-head-callback'       => 'enjoy_vegan_one_header_style',
+			'admin-preview-callback' => 'enjoy_vegan_one_admin_header_image'
 		) ) );
 	}
 	add_action( 'after_setup_theme', 'enjoy_vegan_one_custom_header_setup' );
 
 
-	if ( ! function_exists( 'enjoy_vegan_one_header_style' ) ){
+	if( ! function_exists( 'enjoy_vegan_one_header_style' ) ){
 		/**
 		 * Styles the header image and text displayed on the blog.
 		 *
 		 * @see enjoy_vegan_one_custom_header_setup().
 		 */
-		function enjoy_vegan_one_header_style() {
-			$header_text_color = get_header_textcolor();
 
-			/*
-			 * If no custom options for text are set, let's bail.
-			 * get_header_textcolor() options: Any hex value, 'blank' to hide text. Default: add_theme_support( 'custom-header' ).
-			 */
-			if ( get_theme_support( 'custom-header', 'default-text-color' ) === $header_text_color ) {
-				return;
+	  function enjoy_vegan_one_header_style() {
+		  if ( get_header_image() ) {
+		 	?>
+		 		<style type="text/css">
+		 			.site-header {
+		 				background-image: url(<?php echo get_header_image(); ?>);
+		 			}
+		 		</style>
+		 	<?php
 			}
-
-			// If we get this far, we have custom styles. Let's do this.
-			?>
-				<style type="text/css">
-					<?php　if( get_header_image() ) {　?>
-						<style type="text/css">
-							.site-header {
-								background-image: url(<?php echo get_header_image(); ?>);
-							}
-						</style>
-					<?php　}　?>
-
-					<?php // Has the text been hidden? ?>
-					<?php if( !display_header_text() ){ ?>
-
-						.site-title,
-						.site-description {
-							position: absolute;
-							clip: rect(1px, 1px, 1px, 1px);
-						}
-					<?php // If the user has set a custom color for the text use that. ?>
-					<?php } else { ?>
-						.site-title a,
-						.site-description {
-							color: #<?php echo esc_attr( $header_text_color ); ?>;
-						}
-					<?php	} ?>
-				</style>
-			<?php
 		}
 	}
 ?>
