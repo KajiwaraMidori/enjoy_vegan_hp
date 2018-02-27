3b0c4e43dff873403bf21b34f72eacc96d01dc64
TsubasaK111 <Tsubasa.K111@gmail.com>
2018-02-27 22:30:07 +0900

    style: remove no longer necessary comments

--- wp-content/themes/enjoy_vegan_one/inc/custom-header.php
+++ wp-content/themes/enjoy_vegan_one/inc/custom-header.php
@@ -1,10 +1,5 @@
 <?php
 	/**
-	 * Sample implementation of the Custom Header feature
-	 *
-	 * You can add an optional custom header image to header.php like so ...
-	 *
-		<?php the_header_image_tag(); ?>
 	 *
 	 * @link https://developer.wordpress.org/themes/functionality/custom-headers/
 	 *
