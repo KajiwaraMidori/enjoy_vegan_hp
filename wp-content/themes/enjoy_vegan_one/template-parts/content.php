a6c4480a72f0a8455d1a794f07012f77bbd31255
梶原緑 <kajiwaramidori@kajiwaramidorinoMacBook.local>
2018-03-12 02:43:43 +0900

    投稿の1.タイトル、2.日付、3.投稿者名の3つの項目を削除しました

--- wp-content/themes/enjoy_vegan_one/template-parts/content.php
+++ wp-content/themes/enjoy_vegan_one/template-parts/content.php
@@ -12,17 +12,11 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
-
 		if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
 			<?php
-				enjoy_vegan_one_posted_on();
-				enjoy_vegan_one_posted_by();
+				// enjoy_vegan_one_posted_on();
+				// enjoy_vegan_one_posted_by();
 			?>
 		</div><!-- .entry-meta -->
 		<?php
