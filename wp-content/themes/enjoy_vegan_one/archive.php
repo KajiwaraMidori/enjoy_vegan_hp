050f0bb7c708aec5e4fbd3a6c30178f93f73e72e
midograph <kajiwaramidori@kajiwaramidorinoMacBook.local>
2019-04-09 05:57:57 +0900

    archive title and margin of search field

--- wp-content/themes/enjoy_vegan_one/archive.php
+++ wp-content/themes/enjoy_vegan_one/archive.php
@@ -11,19 +11,20 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-			<div class="category">
+			<h1 class="page-title aligncenter">
 
-		<?php
-		if ( have_posts() ) : ?>
 
-			<header class="page-header">
 				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+				if ( have_posts() ) : ?>
 
-		</div>
+					<header class="page-header">
+						<?php
+							the_archive_title( '<h1 class="page-title">', '</h1>' );
+							the_archive_description( '<div class="archive-description">', '</div>' );
+						?>
+					</header><!-- .page-header -->
+
+			</h1>
 
 			<?php
 			/* Start the Loop */
